07bf0879519f5f93dc130c7b0dbb681353c24dbf
Sam Greenfield-Lee <samgreenfield-lee@Sams-MacBook-Air.local>
2023-11-12 15:35:55 +0000

    updated AirlineAPI lab, added routes (except booking a passenger onto a flight)

--- src/main/java/com/example/airline_api/controllers/FlightController.java
+++ src/main/java/com/example/airline_api/controllers/FlightController.java
@@ -1,7 +1,10 @@
 package com.example.airline_api.controllers;
 
 import com.example.airline_api.models.Flight;
+import com.example.airline_api.models.FlightDTO;
+import com.example.airline_api.services.FlightService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -11,34 +14,46 @@ import java.util.List;
 @RequestMapping("/flights")
 public class FlightController {
 
+    @Autowired
+    private FlightService flightService;
+    private FlightDTO flightDTO;
+
     // Display all available flights
     @GetMapping
-    public ResponseEntity<List<Flight>> getAllFlights(){
-        return null;
+    public ResponseEntity<List<Flight>> getAllFlights(@RequestBody Flight flight)
+    {
+        List<Flight> getAllTheFlights = flightService.findAllFlights();
+        return new ResponseEntity<>(getAllTheFlights, HttpStatus.OK);
     }
 
     // Display a specific flight
     @GetMapping(value = "/{id}")
-    public ResponseEntity<Flight> getFlightById(){
-        return null;
+    public ResponseEntity<Flight> getFlightById(@PathVariable long id)
+    {
+        Flight getSpecificFlight = flightService.findFlight(id);
+        return new ResponseEntity<>(getSpecificFlight, HttpStatus.OK);
     }
 
     // Add details of a new flight
-    @PostMapping
-    public ResponseEntity<Flight> addNewFlight(){
-        return null;
+    @PostMapping("/flights")
+    public ResponseEntity<Flight> addNewFlight(@RequestBody FlightDTO flightDTO)
+    {
+        Flight addedFlight = flightService.addFlight(flightDTO);
+        return new ResponseEntity<>(addedFlight, HttpStatus.CREATED);
     }
 
     // Book passenger on a flight
     @PatchMapping(value = "/{id}")
     public ResponseEntity<Flight> addPassengerToFlight(){
         return null;
-    }
+    } // I understand the theory behind this - grab a passengers ID, assign it to a flight in the FlightDTO, update that flight - however I would have to look at the solutions to actually implement it
 
     // Cancel flight
     @DeleteMapping(value = "/{id}")
-    public ResponseEntity cancelFlight(){
-        return null;
+    public ResponseEntity <Long> cancelFlight (@PathVariable long id)
+    {
+        flightService.deleteFlight(id);
+        return new ResponseEntity<>(id, HttpStatus.OK);
     }
 
 }
--- src/main/java/com/example/airline_api/controllers/PassengerController.java
+++ src/main/java/com/example/airline_api/controllers/PassengerController.java
@@ -1,6 +1,10 @@
 package com.example.airline_api.controllers;
 
 import com.example.airline_api.models.Passenger;
+import com.example.airline_api.repositories.PassengerRepository;
+import com.example.airline_api.services.PassengerService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -10,22 +14,32 @@ import java.util.List;
 @RequestMapping("/passengers")
 public class PassengerController {
 
+
+    @Autowired
+    PassengerService passengerService;
+
     // Display details of all passengers
     @GetMapping
-    public ResponseEntity<List<Passenger>> getAllPassengers(){
-        return null;
+    public ResponseEntity<List<Passenger>> getAllPassengers(@RequestBody Passenger passenger)
+    {
+        List<Passenger> getAllThePassengers = passengerService.findAllPassengers();
+        return new ResponseEntity<>(getAllThePassengers, HttpStatus.OK);
     }
 
     // Display specific passenger details
     @GetMapping(value = "/{id}")
-    public ResponseEntity<Passenger> getPassengerById(){
-        return null;
+    public ResponseEntity<Passenger> getPassengerById(@PathVariable long id)
+    {
+        Passenger getSpecificPassenger = passengerService.findPassenger(id);
+        return new ResponseEntity<>(getSpecificPassenger, HttpStatus.OK);
     }
 
     // Add a new passenger
-    @PostMapping
-    public ResponseEntity<Passenger> addNewPassenger(){
-        return null;
+    @PostMapping("/passengers")
+    public ResponseEntity<Passenger> addNewPassenger(@RequestBody Passenger passenger)
+    {
+        passengerService.addPassenger(passenger);
+        return new ResponseEntity<>(passenger, HttpStatus.CREATED);
     }
 
 }
